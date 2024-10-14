f74613110ad235587d01ad4593719d34bc0bd073
Muhereza Joel <muherezajoel40@gmail.com>
2024-10-14 17:00:20 +0300

    improved dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Indicator;
+use App\Models\Organisation;
+use App\Models\Response;
+use App\Models\TheoryOfChange;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -24,6 +30,18 @@ class DashboardController extends Controller
     public function index()
     {
         $pageTitle = 'Dashboard';
-        return view('dashboard', compact('pageTitle'));
+        $currentUser = Auth::user();
+        $organisation_id = $currentUser->organisation_id;
+
+        $myOrganisation = $myOrganisation = Organisation::findOrFail($organisation_id);
+        $indicatorCount = Indicator::where('organisation_id', $organisation_id)->count();
+        $usersCount = User::where('organisation_id', $organisation_id)->count();
+        $tocCount = TheoryOfChange::where('organisation_id', $organisation_id)->count();
+
+        $responseCount = Response::whereIn('indicator_id', function ($query) use ($organisation_id) {
+            $query->select('id')->from('indicators')->where('organisation_id', $organisation_id);
+        })->count();
+
+        return view('dashboard', compact('pageTitle','myOrganisation', 'indicatorCount', 'responseCount', 'usersCount', 'tocCount'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,29 +1,111 @@
 @include('layouts.header')
+
 @include('layouts.topBar')
+
 @include('layouts.leftPane')
 
-@section('content')
-<section class="section dashboard">
+<main id="main" class="main">
 
+    <section class="section dashboard mt-3">
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="alert alert-warning mt-2 px-3 py-1">
+            <h5>Welcome back, {{ Auth::User()->name }}</h5>
+            <h6 class="fw-bold">| modifying data is allowed to only members of this organisation. Non-members can only view data.</h6>
+            <hr>
+           
+            <h2>Your are a member of  {{ $myOrganisation->name }}</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+        </div>
+
+        @if(Auth::User()->role == 'viewer')
+        <div class="alert alert-danger p-2">
+            {{Auth::User()->name}}, your permissions only allow you to view data. If you need to modify or delete any information, please contact the Administrator to update your permissions.
+        </div>
+        @endif
+
+        <div class="row g-1 mt-4">
+
+            <div class="col-md-3">
+                <div class="row g-1">
+                    <div class="">
+                        <div class="card info-card sales-card">
+                            <div class="card-body">
+                                <h5 class="card-title">All <span>| Users under {{ $myOrganisation->name }}</span></h5>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-graph-up"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{$usersCount}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
-        </div>
-    </div>
 
-</section>
-@endsection
+            <div class="col-md-3">
+                <div class="row g-1">
+                    <div class="">
+                        <div class="card info-card sales-card">
+                            <div class="card-body">
+                                <h5 class="card-title">All <span>| ToCs for {{ $myOrganisation->name }}</span></h5>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-graph-up"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{$tocCount}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="row g-1">
+                    <div class="">
+                        <div class="card info-card sales-card">
+                            <div class="card-body">
+                                <h5 class="card-title">All <span>| Indicators for {{ $myOrganisation->name }}</span></h5>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-graph-up"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{$indicatorCount}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-3">
+                <div class="row g-1">
+                    <div class="">
+                        <div class="card info-card sales-card">
+                            <div class="card-body">
+                                <h5 class="card-title">All <span>| Responses for {{ $myOrganisation->name }}</span></h5>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-graph-up"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{$responseCount}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+</main>
+
 @include('layouts.footer')
