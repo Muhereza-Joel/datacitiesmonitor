d183d007d32da96f8a668f1cb9382b3c1bac6630
Muhereza Joel <muherezajoel40@gmail.com>
2024-10-24 20:49:46 +0300

    test schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('notifications:send-unverified')->hourly();
+        // $schedule->command('notifications:send-unverified')->hourly();
+        $schedule->command('notifications:send-unverified')->everyTenMinutes();
     }
 
     /**
